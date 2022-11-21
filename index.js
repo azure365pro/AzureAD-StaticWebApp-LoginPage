e9d94a42515b1f1b0416f625f8e0d59b7f38dca5
Satheshwaran Manoharan <53287350+azure365pro@users.noreply.github.com>
2022-11-21 14:31:00 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const signUpBtn = document.getElementById('signIn');
 
 signUpBtn.addEventListener('click', () => {
-    window.location.replace("/.auth/login/aad?post_login_redirect_uri=https://virtualpetal.com/authenticated/");
+    window.location.replace("/.auth/login/aad?post_login_redirect_uri=https://zealous-grass-0ea7c0c03.2.azurestaticapps.net/authenticated/");
 });
